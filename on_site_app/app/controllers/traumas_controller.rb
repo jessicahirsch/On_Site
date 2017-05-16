53659cb0e1b6b813874395880fc91e336cd4a806
Jessica Hirsch <jrachelh5@gmail.com>
2017-05-16 12:16:22 -0400

    routes and links for traumas completed

--- on_site_app/app/controllers/traumas_controller.rb
+++ on_site_app/app/controllers/traumas_controller.rb
@@ -1,2 +1,9 @@
 class TraumasController < ApplicationController
+  def index
+    @traumas = Trauma.all
+  end
+
+  def show
+    @trauma = Trauma.find(params[:id])
+  end
 end
--- on_site_app/config/routes.rb
+++ on_site_app/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :traumas
+  resources :first_aid_items
+
+  get "/", to: "home#index"
+  get "/traumas", to: "traumas#index"
+  get "/traumas/:id", to: "traumas#show"
+
+  get "/first_aid", to: "first_aid_items#index"
 end
